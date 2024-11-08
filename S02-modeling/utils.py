28776eb6682f1b4c421a1f799a23b2b957899f18
Celestenono <noemie.moreau96@gmail.com>
2024-11-08 17:15:33 +0100

    debug

--- S02-modeling/utils.py
+++ S02-modeling/utils.py
@@ -316,7 +316,8 @@ def sample_anchors(x, node_mask, ratio, fill_value=0):
     node_mask: [B, N_max]
     """
 
-    print(node_mask)
+    print(node_mask.shape)
+    print(x.shape)
     batch_size = x.size(0)
     num_nodes = node_mask.sum(1)
     max_nodes = num_nodes.max().cpu()
